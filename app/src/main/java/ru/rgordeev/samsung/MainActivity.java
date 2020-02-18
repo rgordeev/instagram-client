00260cfd335f7c5f6981e6d27664d460a94f4638
rgordeev <roma.gordeev@gmail.com>
2020-02-18 19:58:21 +0300

    initial commit

---
+++ app/src/main/java/ru/rgordeev/samsung/MainActivity.java
@@ -0,0 +1,114 @@
+package ru.rgordeev.samsung;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import ru.rgordeev.samsung.model.Person;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.people);
+//        List<String> people = new ArrayList<>();
+//        people.add("Name");
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, people);
+//        listView.setAdapter(adapter);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new HttpThread().execute();
+            }
+        });
+
+    }
+
+    private class HttpThread extends AsyncTask<Void, Void, List<Person>> {
+
+        @Override
+        protected void onPostExecute(List<Person> people) {
+
+            PersonAdapter adapter = new PersonAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, people);
+            listView.setAdapter(adapter);
+        }
+
+        @Override
+        protected List<Person> doInBackground(Void... voids) {
+            OkHttpClient httpClient = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("http://10.0.2.2:8080/person/")
+                    .get()
+                    .build();
+
+            List<Person> people = new ArrayList<>();
+
+            try(Response response = httpClient.newCall(request).execute()) {
+                GsonBuilder gsonBuilder = new GsonBuilder();
+                people = Arrays.asList(gsonBuilder.create().fromJson(response.body().string(), Person[].class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return people;
+        }
+    }
+
+    private static class PersonAdapter extends ArrayAdapter<Person> {
+
+        public PersonAdapter(@NonNull Context context, int resource, @NonNull List<Person> objects) {
+            super(context, resource, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            Person person = getItem(position);
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.person, null);
+
+            PersonHolder holer = new PersonHolder();
+            holer.personName = convertView.findViewById(R.id.personName);
+            holer.personBio = convertView.findViewById(R.id.personBio);
+
+            holer.personName.setText(String.format("%s %s", person.getName(), person.getLastName()));
+            holer.personBio.setText(person.getBio());
+
+            convertView.setTag(holer);
+            return convertView;
+        }
+    }
+
+    private static class PersonHolder {
+        public ImageView avatar;
+        public TextView personName;
+        public TextView personBio;
+    }
+}
